76146e74c95c5bbe2fe719758aac5a3d9cb60e55
desa-thomas <desa2thomas@gmail.com>
2023-11-15 15:54:02 -0500

    included number of bombs in board constructor, updated constructor, and slightly modified bomb generator method

--- src/Board.java
+++ src/Board.java
@@ -6,33 +6,44 @@ class contains 2 2D arrays for minesweeper
 public class Board {
 
     private String[][] gameBoard;
-    private String[][] board;
+    public String[][] board;
     private final int width;
-    private final int height;
+    private final int length;
+    private final int BOMBS;
 
     /*
     constructor for Board class
      */
-    public Board(int width, int height) {
-        this.board = new String[width+1][height+1];
+    public Board(int length, int width, int bombs) {
+        this.board = new String[length +1][width +1];
+        this.length = length;
         this.width = width;
-        this.height = height;
+        this.BOMBS = bombs;
 
-        this.gameBoard = new String[width+1][height+1];
+        this.gameBoard = new String[length +1][width +1];
 
         board[0][0] = "   ";
         gameBoard[0][0] = "   ";
 
-        for (int x = 0; x < width+1; x++) {
-            if(x!=0){
+        for (int x = 0; x < length +1; x++) {
+            if(x!=0 && x < 10){
                 board[x][0] = " " + x + " ";
                 gameBoard[x][0] = " " + x + " ";
             }
-            for (int y = 1; y < this.height+1; y++) {
 
-                if (x == 0){
-                    board[x][y] = " " + (y) + " ";
-                    gameBoard[x][y] = " " + (y) + " ";
+            else if(x != 0){
+                board[x][0] = x +" ";
+                gameBoard[x][0] = x +" ";
+            }
+            for (int y = 1; y < width +1; y++) {
+
+                if (x == 0 && y < 10){
+                    board[0][y] = " " + y + " ";
+                    gameBoard[0][y] = " " + y + " ";
+                }
+                else if(x ==0){
+                    board[0][y] = " " + y;
+                    gameBoard[0][y] = " " + y;
                 }
                 else {
                     gameBoard[x][y] = "[ ]";
@@ -52,10 +63,9 @@ public class Board {
         int randInt, x, y;
         int i = 0;
 
-        while (i < 10) {
-            randInt = r.nextInt(width * height - 1);
-            x = (randInt % width) + 1;
-            y = Math.floorDiv(randInt, height) +1;
+        while (i < BOMBS) {
+            x = r.nextInt(1, length+1);
+            y = r.nextInt(1, width+1);
 
             //if that spot on the board already contains a bomb, do it again
             if (this.board[x][y].equals("[B]")) {
@@ -69,7 +79,24 @@ public class Board {
     }
 
     /*
-    for troubleshooting... prints board with where bombs are
+    reveals tile on gameBoard:
+        1. if tile is a bomb end the game
+        2. If tile is adjacent to bomb(s) put the number of bombs its adjacent to
+        3. If tile is not adjacent to any bomb, keep revealing adjacent tiles until there are no more adjacent tiles
+           with no adjacent bombs
+     */
+    public void revealTile(){}
+
+    /*
+    checks the amount of adjacent bombs to tile with coordinates (x, y)
+    @param x, y coordinates of tile to check
+     */
+    private int checkAdjacency(int x, int y){
+        return 0;
+    }
+
+    /*
+    for troubleshooting... prints hidden board with where bombs are
      */
     public void printBoard() {
 
@@ -86,6 +113,9 @@ public class Board {
 
     }
 
+    /*
+    prints game boards with hidden tiles
+     */
     public void printGameBoard() {
 
         String str = "";
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        Board board = new Board(9, 9);
+        int width = 16;
+        int length = 16;
+        Board board = new Board(length, width, 40);
+
 
         board.printBoard();
-        System.out.println("-".repeat(40));
+        System.out.println("-".repeat(width*4));
         board.printGameBoard();
     }
 }
